47d39203fa561ad17836f9ab7f08ab9b9dfe7404
Annie Kuxa <kuxa.business@gmail.com>
2025-06-26 13:15:30 -0300

    refactor code to use while loop

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-prompt = "Enter a todo: "
-user_text = input(prompt)
-print(user_text)
+user_prompt = "Enter a todo: "
+
+todo_list = []
+
+while True:
+    todo = input(user_prompt)
+    todo_list.append(todo)
+    print(todo_list)
